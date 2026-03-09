f7ad856d9c848083705b5ba22a784bc8ca1031fc
Rigel <112126663+BabyB1u3@users.noreply.github.com>
2026-03-09 16:32:57 -0400

    Extend Shader interface

--- src/graphics/Shader.h
+++ src/graphics/Shader.h
@@ -1,35 +1,71 @@
 #pragma once
 
+#include <cstdint>
+#include <memory>
 #include <string>
+#include <unordered_map>
+
 #include <glm/glm.hpp>
 
-// Shader class
+// TODO: Implement functions
+
 class Shader
 {
 public:
-	// Generate the shader program from the source code
-	Shader(const std::string &filepath, const std::string &name);
-
+	Shader() = default;
 	~Shader();
 
-	void Bind() const;
+	Shader(const Shader &) = delete;
+	Shader &operator=(const Shader &) = delete;
 
+	Shader(Shader &&other) noexcept;
+	Shader &operator=(Shader &&other) noexcept;
+
+	static std::shared_ptr<Shader> CreateFromSource(
+		const std::string &name,
+		const std::string &vertexSource,
+		const std::string &fragmentSource,
+		const std::string &geometrySource = "");
+
+	static std::shared_ptr<Shader> CreateFromFiles(
+		const std::string &name,
+		const std::string &vertexPath,
+		const std::string &fragmentPath,
+		const std::string &geometryPath = "");
+
+	static std::shared_ptr<Shader> CreateFromSingleFile(
+		const std::string &filepath,
+		const std::string &name = "");
+
+	void Bind() const;
 	void Unbind() const;
 
-	const std::string &GetName() const;
+	const std::string &GetName() const { return m_Name; }
+	uint32_t GetRendererID() const { return m_RendererID; }
 
-	void UploadUniformMat4(const std::string &name, const glm::mat4 &value);
-	void UploadUniformMat3(const std::string &name, const glm::mat3 &value);
-	void UploadUniformFloat2(const std::string &name, const glm::vec2 &value);
-	void UploadUniformFloat3(const std::string &name, const glm::vec3 &value);
-	void UploadUniformFloat4(const std::string &name, const glm::vec4 &value);
-	void UploadUniformInt(const std::string &name, const int value);
-	void UploadUniformIntArray(const std::string &name, const int *value, const uint32_t count);
-	void UploadUniformBool(const std::string &name, const bool value);
-	void UploadUniformFloat(const std::string &name, const float value);
+	void SetInt(const std::string &name, int value);
+	void SetIntArray(const std::string &name, const int *values, uint32_t count);
+	void SetBool(const std::string &name, bool value);
+	void SetFloat(const std::string &name, float value);
+	void SetFloat2(const std::string &name, const glm::vec2 &value);
+	void SetFloat3(const std::string &name, const glm::vec3 &value);
+	void SetFloat4(const std::string &name, const glm::vec4 &value);
+	void SetMat3(const std::string &name, const glm::mat3 &value);
+	void SetMat4(const std::string &name, const glm::mat4 &value);
 
 private:
-	std::string m_Name;
+	Shader(uint32_t program, std::string name);
 
-	uint32_t m_Program;
+	static std::string ReadTextFile(const std::string &filepath);
+	static std::unordered_map<uint32_t, std::string> PreProcessSingleFile(const std::string &source);
+
+	static uint32_t CompileStage(uint32_t stage, const std::string &source, const std::string &debugName);
+	static uint32_t LinkProgram(const std::string &name, const std::vector<uint32_t> &shaderIDs);
+
+	int GetUniformLocation(const std::string &name);
+
+private:
+	uint32_t m_RendererID = 0;
+	std::string m_Name;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
 };
