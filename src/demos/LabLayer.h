169f81b3437bc7ee31b44340bc1ca349fefaec32
Rigel <112126663+BabyB1u3@users.noreply.github.com>
2026-03-10 17:26:39 -0400

    Implement LabLayer

---
+++ src/demos/LabLayer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "core/Layer.h"
+#include "core/Base.h"
+
+class DemoBase;
+
+class LabLayer : public Layer
+{
+public:
+    LabLayer();
+
+    void OnAttach() override;
+    void OnDetach() override;
+
+    void OnUpdate(float dt) override;
+    void OnRender() override;
+    void OnImGuiRender() override;
+    void OnResize(uint32_t width, uint32_t height) override;
+
+private:
+    void RegisterBuiltInDemos();
+    void SetActiveDemo(Scope<DemoBase> demo, const std::string &name);
+
+private:
+    bool m_DemosRegistered = false;
+
+    std::string m_ActiveDemoName;
+    Scope<DemoBase> m_ActiveDemo;
+};
