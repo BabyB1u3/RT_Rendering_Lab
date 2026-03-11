b86d8b1c2ed41297450db07da5a32fd0eb4777f8
Rigel <112126663+BabyB1u3@users.noreply.github.com>
2026-03-11 18:48:25 -0400

    Migrate OpenGL func calls to DSA in Buffers

--- src/graphics/Buffers.cpp
+++ src/graphics/Buffers.cpp
@@ -8,16 +8,14 @@ VertexBuffer::VertexBuffer(uint32_t size, BufferUsage usage)
 	: m_Usage(usage)
 {
 	glCreateBuffers(1, &m_RendererID);
-	glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
-	glBufferData(GL_ARRAY_BUFFER, size, nullptr, ToOpenGLBufferUsage(m_Usage));
+	glNamedBufferData(m_RendererID, size, nullptr, ToOpenGLBufferUsage(usage));
 }
 
 VertexBuffer::VertexBuffer(const void *data, uint32_t size, BufferUsage usage)
 	: m_Usage(usage)
 {
 	glCreateBuffers(1, &m_RendererID);
-	glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
-	glBufferData(GL_ARRAY_BUFFER, size, data, ToOpenGLBufferUsage(m_Usage));
+	glNamedBufferData(m_RendererID, size, data, ToOpenGLBufferUsage(usage));
 }
 
 VertexBuffer::~VertexBuffer()
@@ -62,8 +60,7 @@ void VertexBuffer::Unbind() const
 
 void VertexBuffer::SetData(const void *data, uint32_t size, uint32_t offset)
 {
-	glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
-	glBufferSubData(GL_ARRAY_BUFFER, offset, size, data);
+	glNamedBufferSubData(m_RendererID, offset, size, data);
 }
 
 // -------------------- IndexBuffer --------------------
@@ -72,8 +69,7 @@ IndexBuffer::IndexBuffer(const uint32_t *indices, uint32_t count)
 	: m_Count(count)
 {
 	glCreateBuffers(1, &m_RendererID);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+	glNamedBufferData(m_RendererID, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
 }
 
 IndexBuffer::~IndexBuffer()
