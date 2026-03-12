e7fb5f00fda3993ae2f9222a79080577d27c1b4b
Rigel <112126663+BabyB1u3@users.noreply.github.com>
2026-03-12 16:31:06 -0400

    Add 3x3 PCF for soft shadow edges

--- assets/shaders/ForwardLit.glsl
+++ assets/shaders/ForwardLit.glsl
@@ -54,11 +54,21 @@ float ComputeShadow(vec4 lightSpacePosition, vec3 normal, vec3 lightDir)
     if (projCoords.z > 1.0)
         return 0.0;
 
-    float closestDepth = texture(u_ShadowMap, projCoords.xy).r;
     float currentDepth = projCoords.z;
-
     float bias = max(0.0005, 0.005 * (1.0 - dot(normalize(normal), normalize(-lightDir))));
-    float shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
+
+    // 3x3 PCF for soft shadow edges
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(u_ShadowMap, 0);
+    for (int x = -1; x <= 1; ++x)
+    {
+        for (int y = -1; y <= 1; ++y)
+        {
+            float pcfDepth = texture(u_ShadowMap, projCoords.xy + vec2(x, y) * texelSize).r;
+            shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;
+        }
+    }
+    shadow /= 9.0;
 
     return shadow;
 }
