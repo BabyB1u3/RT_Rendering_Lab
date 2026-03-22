27a26b1f40036c288b06994b322037ead20002c1
Rigel <112126663+BabyB1u3@users.noreply.github.com>
2026-03-22 16:31:12 -0400

    Optimize building speed

---
+++ src/pch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <filesystem>
+#include <memory>
+#include <optional>
+#include <span>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <spdlog/spdlog.h>
