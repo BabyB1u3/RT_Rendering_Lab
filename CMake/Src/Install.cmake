c11643ad8cfc89d573a241c67a5ff2d9dfad91f4
Rigel <112126663+BabyB1u3@users.noreply.github.com>
2026-04-11 17:09:16 -0400

    Unify cmake target, preset naming

--- CMake/Src/Install.cmake
+++ CMake/Src/Install.cmake
@@ -5,18 +5,18 @@ install(CODE "
     endif()
 
     set(stage_dir \"${CMAKE_BINARY_DIR}/Stage/\${stage_config}\")
-    set(stage_runtime \"${GLAB_STAGE_SHIPPING_RUNTIME_NAME}\")
+    set(stage_runtime \"${GLAB_STAGE_RUNTIME_NAME}\")
     set(stage_pak \"\${stage_dir}/Game.rtrpak\")
     set(stage_runtime_path \"\${stage_dir}/\${stage_runtime}\")
 
     if(NOT EXISTS \"\${stage_pak}\")
         message(FATAL_ERROR
-            \"Missing staged shipping assets at '\${stage_pak}'. Build target 'rtrlab_stage_shipping' before running install.\")
+            \"Missing packaged runtime content at '\${stage_pak}'. Build target 'rtrlab_stage_runtime' before running install.\")
     endif()
 
     if(NOT EXISTS \"\${stage_runtime_path}\")
         message(FATAL_ERROR
-            \"Missing staged runtime at '\${stage_runtime_path}'. Build target 'rtrlab_stage_shipping' before running install.\")
+            \"Missing staged runtime at '\${stage_runtime_path}'. Build target 'rtrlab_stage_runtime' before running install.\")
     endif()
 
     file(INSTALL DESTINATION \"${CMAKE_INSTALL_PREFIX}\" TYPE EXECUTABLE
--- CMake/Src/Packaging.cmake
+++ CMake/Src/Packaging.cmake
@@ -1,29 +1,29 @@
-set(GLAB_STAGE_SHIPPING_COOKED_DIR "${CMAKE_BINARY_DIR}/Packaging/$<CONFIG>/Cooked")
-set(GLAB_STAGE_SHIPPING_DIR "${CMAKE_BINARY_DIR}/Stage/$<CONFIG>")
-set(GLAB_STAGE_SHIPPING_RUNTIME_NAME "$<TARGET_FILE_NAME:RTRLab>")
+set(GLAB_PACKAGE_CONTENT_COOKED_DIR "${CMAKE_BINARY_DIR}/Packaging/$<CONFIG>/Cooked")
+set(GLAB_STAGE_RUNTIME_DIR "${CMAKE_BINARY_DIR}/Stage/$<CONFIG>")
+set(GLAB_STAGE_RUNTIME_NAME "$<TARGET_FILE_NAME:RTRLab>")
 
-add_custom_target(rtrlab_stage_shipping_assets
-    COMMAND ${CMAKE_COMMAND} -E make_directory "${GLAB_STAGE_SHIPPING_COOKED_DIR}"
-    COMMAND ${CMAKE_COMMAND} -E make_directory "${GLAB_STAGE_SHIPPING_DIR}"
+add_custom_target(rtrlab_package_content
+    COMMAND ${CMAKE_COMMAND} -E make_directory "${GLAB_PACKAGE_CONTENT_COOKED_DIR}"
+    COMMAND ${CMAKE_COMMAND} -E make_directory "${GLAB_STAGE_RUNTIME_DIR}"
     COMMAND $<TARGET_FILE:rtr_asset_cook>
             --root "${CMAKE_SOURCE_DIR}"
-            --out "${GLAB_STAGE_SHIPPING_COOKED_DIR}"
+            --out "${GLAB_PACKAGE_CONTENT_COOKED_DIR}"
     COMMAND $<TARGET_FILE:rtr_asset_pack>
-            --source "${GLAB_STAGE_SHIPPING_COOKED_DIR}"
-            --out "${GLAB_STAGE_SHIPPING_DIR}"
+            --source "${GLAB_PACKAGE_CONTENT_COOKED_DIR}"
+            --out "${GLAB_STAGE_RUNTIME_DIR}"
     DEPENDS
         rtr_asset_cook
         rtr_asset_pack
-    COMMENT "Cooking and packaging shipping assets into ${GLAB_STAGE_SHIPPING_DIR}"
+    COMMENT "Packaging runtime content into ${GLAB_STAGE_RUNTIME_DIR}"
 )
 
-add_custom_target(rtrlab_stage_shipping
-    COMMAND ${CMAKE_COMMAND} -E make_directory "${GLAB_STAGE_SHIPPING_DIR}"
+add_custom_target(rtrlab_stage_runtime
+    COMMAND ${CMAKE_COMMAND} -E make_directory "${GLAB_STAGE_RUNTIME_DIR}"
     COMMAND ${CMAKE_COMMAND} -E copy_if_different
             $<TARGET_FILE:RTRLab>
-            "${GLAB_STAGE_SHIPPING_DIR}/$<TARGET_FILE_NAME:RTRLab>"
+            "${GLAB_STAGE_RUNTIME_DIR}/$<TARGET_FILE_NAME:RTRLab>"
     DEPENDS
         RTRLab
-        rtrlab_stage_shipping_assets
-    COMMENT "Staging shipping runtime into ${GLAB_STAGE_SHIPPING_DIR}"
+        rtrlab_package_content
+    COMMENT "Staging runtime into ${GLAB_STAGE_RUNTIME_DIR}"
 )
