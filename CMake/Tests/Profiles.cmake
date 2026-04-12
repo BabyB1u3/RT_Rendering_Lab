0561db06e0744716a5dc71f785040188c5ee4400
Rigel <112126663+BabyB1u3@users.noreply.github.com>
2026-04-11 21:42:18 -0400

    Split profile targets by build type

--- CMake/Tests/Profiles.cmake
+++ CMake/Tests/Profiles.cmake
@@ -4,15 +4,17 @@ set(GLAB_DEV_PROFILE_TEST_SOURCES
     Dev/Integration/Core/Resource/TestRootDiscoveryDev.cpp
 )
 
-glab_add_test_executable(rtrlab_dev_profile_tests "DevProfile." "integration;dev-profile"
-    ${GLAB_DEV_PROFILE_TEST_SOURCES}
-)
-
 set(GLAB_SHIPPING_PROFILE_TEST_SOURCES
     Shipping/Integration/Core/Resource/TestMountDiscoveryShipping.cpp
     Shipping/Integration/Core/Resource/TestRootDiscoveryShipping.cpp
 )
 
-glab_add_test_executable(rtrlab_shipping_profile_tests "ShippingProfile." "integration;shipping-profile"
-    ${GLAB_SHIPPING_PROFILE_TEST_SOURCES}
-)
+if(CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+    glab_add_test_executable(rtrlab_dev_profile_tests "DevProfile." "integration;dev-profile"
+        ${GLAB_DEV_PROFILE_TEST_SOURCES}
+    )
+elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
+    glab_add_test_executable(rtrlab_shipping_profile_tests "ShippingProfile." "integration;shipping-profile"
+        ${GLAB_SHIPPING_PROFILE_TEST_SOURCES}
+    )
+endif()
